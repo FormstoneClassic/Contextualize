c3a42797365c1a6ac8c5ef0157f3a54ab1f2bb1c
Ben Plum <mr@benplum.com>
2012-09-22 23:48:24 -0400

    Refactored for performance and smaller file size;  Replaced .each() with native for loop

--- jquery.bp.contextualize.js
+++ jquery.bp.contextualize.js
@@ -1,13 +1,13 @@
 /*
  * Contextualize Plugin - Give your links some context
  * @author Ben Plum
- * @version 0.2
+ * @version 0.4
  *
  * Copyright © 2012 Ben Plum <mr@benplum.com>
  * Released under the MIT License <http://www.opensource.org/licenses/mit-license.php>
  */
  
-(function($) {
+if (jQuery) (function($) {
 	
 	// Default Options
 	var options = {
@@ -16,36 +16,12 @@
 		types: ["pdf", "doc", "xls", "ppt", "swf", "zip", "mp3"]
 	};
 	
-	// Methods
-	var methods = {
+	// Public Methods
+	var pub = {
 		
 		// Set / Update defaults
 		defaults: function(opts) {
 			options = jQuery.extend(options, opts);
-			return $(this);
-		},
-		
-		// Add some context
-		init: function(opts) {
-			options = jQuery.extend(options, opts);
-			var filter = (options.exclude != "") ? ", " + options.exclude : "";
-			
-			$(this).not('[context="on"]' + filter).each(function() {
-				var $anchor = $(this);
-				if ($anchor.attr("href") !== undefined) {
-					var classes = "";
-					var ext = $anchor.attr("href").substr(-3).toLowerCase();
-					if ($.inArray(ext, options.types) > -1) {
-						classes += " " + options.prefix + ext;
-					}
-					if ($anchor.attr("target") == "_blank") {
-						classes += " " + options.prefix + "external";
-					}
-					
-					$anchor.addClass(classes).attr("context", "on");
-				}
-			});
-			
 			return $(this);
 		},
 		
@@ -57,14 +33,39 @@
 		}
 	};
 	
+	// Add some context
+	function _init(opts) {
+		options = jQuery.extend(options, opts);
+		var filter = (options.exclude != "") ? ", " + options.exclude : "";
+		
+		var $items = $(this).not('[context="on"]' + filter);
+		var counter = $items.lenth;
+		for (var i = 0, count = $items.length; i < count; i++) {
+			var $anchor = $items.eq(i);
+			if ($anchor.attr("href") !== undefined) {
+				var classes = "";
+				var ext = $anchor.attr("href").substr(-3).toLowerCase();
+				if ($.inArray(ext, options.types) > -1) {
+					classes += " " + options.prefix + ext;
+				}
+				if ($anchor.attr("target") == "_blank") {
+					classes += " " + options.prefix + "external";
+				}
+				
+				$anchor.addClass(classes).attr("context", "on");
+			}
+		}
+		
+		return $items;
+	}
+	
 	// Define Plugin 
 	$.fn.contextualize = function(method) {
-		if (methods[method]) {
-			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		if (pub[method]) {
+			return pub[method].apply(this, Array.prototype.slice.call(arguments, 1));
 		} else if (typeof method === 'object' || !method) {
-			return methods.init.apply(this, arguments);
-		} else {
-			$.error("CONTEXTUALIZE: '" +  method + "' does not exist.");
+			return _init.apply(this, arguments);
 		}
+		return this;
 	};
 })(jQuery); 
